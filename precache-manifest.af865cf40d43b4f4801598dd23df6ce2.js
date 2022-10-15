81a455f318cab57defc810d306db934abc350b61
punyeeet <puneetpradhan007@gmail.com>
2022-10-15 18:49:51 +0530

    Updates

---
+++ precache-manifest.af865cf40d43b4f4801598dd23df6ce2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a282bb6ecb35acb4fd9",
+    "url": "/todo-list/static/css/main.638171a3.chunk.css"
+  },
+  {
+    "revision": "4a282bb6ecb35acb4fd9",
+    "url": "/todo-list/static/js/main.4a282bb6.chunk.js"
+  },
+  {
+    "revision": "bfaef02fc73415496900",
+    "url": "/todo-list/static/js/1.bfaef02f.chunk.js"
+  },
+  {
+    "revision": "b1f46cf93ae039b64652",
+    "url": "/todo-list/static/js/2.b1f46cf9.chunk.js"
+  },
+  {
+    "revision": "9f1969b62c28826fe926",
+    "url": "/todo-list/static/js/runtime~main.9f1969b6.js"
+  },
+  {
+    "revision": "42f0e1ef260cd73ba5070a63af5437dc",
+    "url": "/todo-list/index.html"
+  }
+];
